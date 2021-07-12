8f82bb5719c4c8a2b59472f9d993247e2fdb8d71
crissvilleda <crisswebapp@gmail.com>
2021-07-11 19:48:11 -0600

    ADDED: se elimino codigo que no se usa

--- frontend/src/js/redux/modules/pagos/pagos.js
+++ frontend/src/js/redux/modules/pagos/pagos.js
@@ -29,33 +29,9 @@ const setLoader = (loader) => ({
     loader,
 });
 
-const setData = (data) => ({
-    type: constants.DATA,
-    data,
-});
-const setData2 = (data2) => ({
-    type: constants.DATA2,
-    data2,
-});
-
 const setItem = (item) => ({
     type: constants.ITEM,
     item,
-});
-
-const setPage = (page) => ({
-    type: constants.PAGE,
-    page,
-});
-
-const setOrdering = (ordering) => ({
-    type: constants.ORDERING,
-    ordering,
-});
-
-const setSearch = (search) => ({
-    type: constants.SEARCH,
-    search,
 });
 
 const setMeses = (meses) => ({
@@ -67,27 +43,6 @@ const setMeses = (meses) => ({
 // Actions
 // -----------------------------------
 
-const listar =
-    (page = 1, tipo = undefined) =>
-    (dispatch, getStore) => {
-        const resource = getStore().servicios;
-        const params = { page };
-        params.ordering = resource.ordering;
-        params.search = resource.search;
-        if (tipo) params.tipo = tipo;
-        dispatch(setLoader(true));
-        api.get('servicio', params)
-            .then((response) => {
-                if (tipo === AGUA) dispatch(setData(response));
-                if (tipo === CEMENTERIO) dispatch(setData2(response));
-                dispatch(setPage(page));
-            })
-            .catch(() => {})
-            .finally(() => {
-                dispatch(setLoader(false));
-            });
-    };
-
 const leer = (id) => (dispatch) => {
     dispatch(setLoader(true));
     api.get(`servicio/${id}`)
@@ -96,42 +51,6 @@ const leer = (id) => (dispatch) => {
             dispatch(initializeForm('servicioForm', response));
         })
         .catch(() => {})
-        .finally(() => {
-            dispatch(setLoader(false));
-        });
-};
-
-const crear = (data) => (dispatch) => {
-    dispatch(setLoader(true));
-    api.post('servicio', data)
-        .then(() => {
-            NotificationManager.success('Registro creado', 'Éxito', 3000);
-            if (data.tipo === AGUA) dispatch(push('/usuarios/agua'));
-            if (data.tipo === CEMENTERIO)
-                dispatch(push('/usuarios/cementerio'));
-        })
-        .catch((error) => {
-            let msj = 'Error en la creación';
-            if (error && error.dpi) msj = error.dpi;
-            NotificationManager.error(msj, 'ERROR');
-        })
-        .finally(() => {
-            dispatch(setLoader(false));
-        });
-};
-
-const editar = (id, data) => (dispatch) => {
-    dispatch(setLoader(true));
-    api.put(`servicio/${id}`, data)
-        .then(() => {
-            NotificationManager.success('Registro actualizado', 'Éxito', 3000);
-            if (data.tipo === AGUA) dispatch(push('/usuarios/agua'));
-            if (data.tipo === CEMENTERIO)
-                dispatch(push('/usuarios/cementerio'));
-        })
-        .catch(() => {
-            NotificationManager.error('Error en la edición', 'ERROR', 0);
-        })
         .finally(() => {
             dispatch(setLoader(false));
         });
@@ -161,48 +80,8 @@ const obtenerMeses = (id, anio) => (dispatch, getStore) => {
         });
 };
 
-const eliminar = (id) => (dispatch) => {
-    dispatch(setLoader(true));
-    api.eliminar(`servicio/${id}`)
-        .then(() => {
-            dispatch(listar());
-            NotificationManager.success('Registro eliminado', 'Éxito', 3000);
-        })
-        .catch(() => {
-            NotificationManager.success(
-                'Error en la transacción',
-                'Éxito',
-                3000
-            );
-        })
-        .finally(() => {
-            dispatch(setLoader(false));
-        });
-};
-
-const searchChange = (search) => (dispatch) => {
-    dispatch(setSearch(search));
-    dispatch(listar());
-};
-
-const onSortChange = (ordering) => (dispatch, getStore) => {
-    const sort = getStore().servicios.ordering;
-    if (ordering === sort) {
-        dispatch(setOrdering(`-${ordering}`));
-    } else {
-        dispatch(setOrdering(ordering));
-    }
-    dispatch(listar());
-};
-
 export const actions = {
-    listar,
     leer,
-    crear,
-    editar,
-    eliminar,
-    searchChange,
-    onSortChange,
     obtenerMeses,
 };
 
